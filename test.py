a06a7da7c4777b13c8f9c5b2f5fa31983d3a97e5
siwar-lachiheb <98648491+siwar-lachiheb@users.noreply.github.com>
2022-06-15 04:25:22 +0200

    test.py

--- test.py
+++ test.py
@@ -61,15 +61,10 @@ import shutil
 
 def splitting (root_dir,src) :
     # # Creating Train / Val / Test folders (One time use)
-    #posCls = '/DPN+'
-    #negCls = '/DPN-'
 
     os.makedirs(root_dir +'/train')
-    #os.makedirs(root_dir +'/train' + negCls)
     os.makedirs(root_dir +'/val')
-    #os.makedirs(root_dir +'/val' + negCls)
     os.makedirs(root_dir +'/test')
-    #os.makedirs(root_dir +'/test' + negCls)
 
     # Creating partitions of the data after shuffeling
 
@@ -100,69 +95,14 @@ def splitting (root_dir,src) :
 
 
 #Building the CNN model ----------------------------------------------------------------------------------------------------------------------------------------------------------------------
-
-
-#importing models
-import numpy as np
-#matplotlib inline
-import matplotlib.image as mpimg
-import matplotlib.pyplot as plt
-import tensorflow as tf
-tf.compat.v1.set_random_seed(2019)
-#form the cnn model
-model = tf.keras.models.Sequential([
-    tf.keras.layers.Conv2D(16,(3,3),activation = "relu" , input_shape = (180,180,3)) ,
-    tf.keras.layers.MaxPooling2D(2,2),
-    tf.keras.layers.Conv2D(32,(3,3),activation = "relu") ,  
-    tf.keras.layers.MaxPooling2D(2,2),
-    tf.keras.layers.Conv2D(64,(3,3),activation = "relu") ,  
-    tf.keras.layers.MaxPooling2D(2,2),
-    tf.keras.layers.Conv2D(128,(3,3),activation = "relu"),  
-    tf.keras.layers.MaxPooling2D(2,2),
-    tf.keras.layers.Flatten(), 
-    tf.keras.layers.Dense(550,activation="relu"),      #Adding the Hidden layer
-    tf.keras.layers.Dropout(0.1,seed = 2019),
-    tf.keras.layers.Dense(400,activation ="relu"),
-    tf.keras.layers.Dropout(0.3,seed = 2019),
-    tf.keras.layers.Dense(300,activation="relu"),
-    tf.keras.layers.Dropout(0.4,seed = 2019),
-    tf.keras.layers.Dense(200,activation ="relu"),
-    tf.keras.layers.Dropout(0.2,seed = 2019),
-    tf.keras.layers.Dense(5,activation = "softmax")   #Adding the Output Layer
-])
-
-#print the model summary
-
-model.summary()
-
-# specify optimizers
-
-from tensorflow.keras.optimizers import RMSprop,SGD,Adam
-adam=Adam(learning_rate=0.001)
-model.compile(optimizer='adam', loss='categorical_crossentropy', metrics = ['acc'])#Optimizer is used to reduce the cost calculated by cross-entropy
-
-
-#set the data directory and generate images
-bs=30         #Setting batch size
-train_dir = "C:/Users/lenovo/Desktop/stage/output/train/"   #Setting training directory
-validation_dir = "C:/Users/lenovo/Desktop/stage/output/test/"   #Setting testing directory
-from tensorflow.keras.preprocessing.image import ImageDataGenerator 
-# All images will be rescaled by 1./255.
-train_datagen = ImageDataGenerator( rescale = 1.0/255. )
-test_datagen  = ImageDataGenerator( rescale = 1.0/255. )
-# Flow training images in batches of 20 using train_datagen generator
-#Flow_from_directory function lets the classifier directly identify the labels from the name of the directories the image lies in
-train_generator=train_datagen.flow_from_directory(train_dir,batch_size=bs,class_mode='categorical',target_size=(180,180))
-# Flow validation images in batches of 20 using test_datagen generator
-validation_generator =  test_datagen.flow_from_directory(validation_dir,
-                                                         batch_size=bs,
-                                                         class_mode  = 'categorical',
-                                                         target_size=(180,180))
-
-###fitting the model
-history = model.fit(train_generator,
-                    validation_data=validation_generator,
-                    steps_per_epoch=150 // bs,
-                    epochs=30,
-                    validation_steps=50 // bs,
-                    verbose=2)
+#resizing images 
+import PIL
+import os
+from PIL import Image
+directories=['d0','d1','d2']
+for d in directories:
+  for file in os.listdir(d):
+    d_img = d+"/"+file
+    img = Image.open(d_img)
+    img = img.resize((64,64))
+    img.save(d_img)
